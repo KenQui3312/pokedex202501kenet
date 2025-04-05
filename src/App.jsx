d0f0d71e64ae90f257d45c2fcc3f451ba19c813e
KenQui3312 <kenetadiel@gmail.com>
2025-04-04 19:47:06 -0600

    commit8

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   
   return (
     
-    <BrowserRouter basename={import.meta.env.DEPLOY_BASE_URL ?? '/'}>
+    <BrowserRouter basename={import.meta.env.DEPLOY_BASE_URL ?? './'}>
       <Routes  >
         <Route element={<Layout />}>
           <Route index element={<DashboardPage/>} />
